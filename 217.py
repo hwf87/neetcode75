0ac8f2edd8577908f74a54ade84f1709ccc85c7b
jackyfu <jackyfu1995@gmail.com>
2022-10-16 12:35:06 +0800

    leetcode 217

---
+++ 217.py
@@ -0,0 +1,21 @@
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        '''
+        TC = O(n)
+        SC = O(n)
+        '''
+        return len(set(nums)) != len(nums)
+    
+# Unit Test
+import pytest
+class TestCase(Solution):
+    @pytest.mark.parametrize("test_input, expect", \
+        [([1,1,2,3], True), 
+         ([1,2,3,4], False),
+         ([1], False)]
+    )
+    def test_containsDuplicate(self, test_input, expect):
+        '''
+        '''
+        answer = self.containsDuplicate(test_input)
+        assert answer == expect
