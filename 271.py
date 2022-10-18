015ccc36e08cfa8173d4269b33c727e748d0a2b3
jackyfu <jackyfu1995@gmail.com>
2022-10-18 23:23:00 +0800

    leetcode 271

---
+++ 271.py
@@ -0,0 +1,46 @@
+# 659. Encode and Decode Strings
+class Solution:
+    """
+    @param: strs: a list of strings
+    @return: encodes a list of strings to a single string.
+    """
+    def encode(self, strs):
+        # write your code here
+        res = ""
+        for s in strs:
+            newstr = str(len(s)) + "$" + s
+            res += newstr
+        return res
+
+    """
+    @param: str: A string
+    @return: dcodes a single string to a list of strings
+    """
+    def decode(self, str):
+        # write your code here
+        
+        return ""
+
+# Unit Test
+import pytest
+class TestCase(Solution):
+    @pytest.mark.parametrize("strs, expect", \
+                             [(["lint", "code", "love", "you"], "4$lint4$code4$love3$you"),
+                              (["5$aa", "aa2$aa"], "4$5$aa6$aa2$aa")]
+                            )
+    def test_encode(self, strs, expect):
+        '''
+        '''
+        answer = self.encode(strs)
+        assert answer == expect
+
+    @pytest.mark.parametrize("strs, expect", \
+                             [("4$lint4$code4$love3$you", ["lint", "code", "love", "you"]),
+                              ("4$5$aa6$aa2$aa", ["5$aa", "aa2$aa"])]
+                            )
+    def test_decode(self, strs, expect):
+        '''
+        '''
+        answer = self.encode(strs)
+        assert answer == expect
+    
