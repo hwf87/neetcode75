092cba394e12e56bdee2c248206784b327ecf137
jackyfu <jackyfu1995@gmail.com>
2022-11-05 23:55:28 +0800

    leetcode 572

---
+++ 572.py
@@ -0,0 +1,13 @@
+# 572. Subtree of Another Tree
+# Definition for a binary tree node.
+from typing import Optional 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        '''
+        '''
+        
