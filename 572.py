2ebddc8a873d4964b5f068d2e3517cbe3c2d845d
jackyfu <jackyfu1995@gmail.com>
2022-11-06 11:43:15 +0800

    leetcode 572 implement isSametree methods

--- 572.py
+++ 572.py
@@ -13,8 +13,23 @@ class Solution:
         
         return ''
     
-    def isSametree(self, s: Optional[TreeNode], t: Optional[TreeNode]) -> bool:
+    def isSametree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         '''
+        Function to check if two trees have same structure
         '''
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        
+        Left = self.isSametree(p.left, q.left)
+        Right = self.isSametree(p.right, q.right)
+        isSame = Left & Right
+        return isSame
+
+
+
         return ''
 
