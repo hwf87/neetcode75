7d1041685def5a53eef3bffdb7611b6d8989e2ba
jackyfu <jackyfu1995@gmail.com>
2022-11-12 12:34:44 +0800

    hash defaultdict solution

--- 36.py
+++ 36.py
@@ -1,12 +1,31 @@
 # 36. Valid Sudoku
+import collections
 from typing import List
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         '''
+        TC = O(n*2)
+        SC = O(n)
         '''
-
-
-
+        row_dict = collections.defaultdict(set)
+        column_dict = collections.defaultdict(set)
+        box_dict = collections.defaultdict(set)
+        
+        for r in range(0, 9):
+            for c in range(0, 9):
+                if board[r][c] == '.':
+                    continue
+                elif (
+                    board[r][c] in row_dict[r] or
+                    board[r][c] in column_dict[c] or
+                    board[r][c] in box_dict[(r//3, c//3)]
+                ):
+                    return False
+                else:
+                    row_dict[r].add(board[r][c])
+                    column_dict[c].add(board[r][c])
+                    box_dict[(r//3, c//3)].add(board[r][c])
+        return True
 
 # Unit test
 import pytest
